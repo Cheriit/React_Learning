90860f54956dec6b30376c57826f40d8367a7f79
Filip Szóstak <filip-szostak@wp.pl>
2019-07-02 21:39:14 +0200

    Setting up structure and routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import StreamShow from './streams/SteramShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import Header from './Header';
 
 export const App = () => {
-	return <p>Tets</p>;
+	return (
+		<div className='ui container'>
+			<HashRouter>
+				<Header />
+				<div>
+					<Route path='/' exact component={StreamList} />
+					<Route path='/streams/new' component={StreamCreate} />
+					<Route path='/streams/edit' component={StreamEdit} />
+					<Route path='/streams/delete' component={StreamDelete} />
+					<Route path='/streams/show' component={StreamShow} />
+				</div>
+			</HashRouter>
+		</div>
+	);
 };
 
 export default App;
