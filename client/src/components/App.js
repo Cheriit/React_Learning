4a0da78ec1ce34f7a202828acabfe5026d8c14ba
Filip Szóstak <filip-szostak@wp.pl>
2019-07-07 23:22:46 +0200

    editing and creating streams

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+
+import history from '../history';
 import StreamShow from './streams/SteramShow';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -10,16 +12,20 @@ import Header from './Header';
 export const App = () => {
 	return (
 		<div className='ui container'>
-			<HashRouter>
+			<Router history={history}>
 				<Header />
 				<div>
 					<Route path='/' exact component={StreamList} />
-					<Route path='/streams/new' component={StreamCreate} />
-					<Route path='/streams/edit' component={StreamEdit} />
-					<Route path='/streams/delete' component={StreamDelete} />
-					<Route path='/streams/show' component={StreamShow} />
+					<Route path='/streams/new' exact component={StreamCreate} />
+					<Route path='/streams/edit/:id' exact component={StreamEdit} />
+					<Route
+						path='/streams/delete/:id'
+						exact
+						component={StreamDelete}
+					/>
+					<Route path='/streams/show' exact component={StreamShow} />
 				</div>
-			</HashRouter>
+			</Router>
 		</div>
 	);
 };
